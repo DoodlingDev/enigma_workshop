f6bcc6d2571d837e861dbac8c44a13d706e27ac5
Aji Slater <aji.slater@gmail.com>
2024-11-04 14:04:18 -0600

    Adds tests for encypering a single character
    Two tests are added to cover the initial most basic functionality of the
    machine.
    
    a. Can it change one character into another?
    
    b. Does it return a different character for each letter?

--- enigma_test.rb
+++ enigma_test.rb
@@ -2,4 +2,16 @@ require "minitest/autorun"
 require_relative "enigma"
 
 class Enigma::Test < Minitest::Test
+  def test_encyphers_a_character
+    enigma = Enigma.new
+    result = enigma.cypher("A")
+    refute_equal "A", result
+    assert_kind_of String, result
+  end
+
+  def test_encyphers_different_characters_for_each
+    enigma = Enigma.new
+    encyphered_character = enigma.cypher("A")
+    refute_equal encyphered_character, enigma.cypher("B")
+  end
 end
