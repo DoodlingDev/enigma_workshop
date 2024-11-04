ed612d3387e9a3b58f4c7904cf24365564671b8a
Aji Slater <aji.slater@gmail.com>
2024-11-04 14:06:17 -0600

    Implements single character cyphering
    Using the basic configuration of the Enigma machine as a guide, we'll
    use three rotors to implement a simple substitution (caesar) cypher.
    
    The input character is changed to an integer, mutated three times with
    the rotor, and translated back to a letter from a number.
    
    We're using the array method `at` here instead of what we might usually
    do with using the `[]` access of an array index. This is to highlight
    the structure of the messages being passed. It's much easier to think of
    `.at` being a message passed from the Enigma's cypher method to an array
    (the rotor), than `ROTOR_I[index]`.
    
    >> Even if the `[]` accessor is also a method call equivalent to
    `ROTOR_I.[](index)`. 😜

--- enigma.rb
+++ enigma.rb
@@ -1,6 +1,15 @@
-require_relative "wiring.rb"
+require_relative "wiring"
 
 class Enigma
+  ALPHABET = ("A".."Z").to_a
+
   def cypher(character)
+    character_index = ALPHABET.index(character)
+
+    shifted_index = ROTOR_I.at(character_index)
+    shifted_index = ROTOR_II.at(shifted_index)
+    shifted_index = ROTOR_III.at(shifted_index)
+
+    ALPHABET.at(shifted_index)
   end
 end
