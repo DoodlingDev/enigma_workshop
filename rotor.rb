60783ada9775b63d88047a2cbb3887a71792ac2b
Aji Slater <aji.slater@gmail.com>
2024-11-05 13:41:10 -0600

    Adds position to Rotor class
    In order for the rotor to return different characters along the length
    of its key, it is going to have to store how many times it has been used
    (or the equivalent) to know how much to step forward.
    
    Now our rotors will move forward one index in their keys after each call
    to `#translate`.
    
    The combination of related behavior and state is the core idea of an
    object, or a class. Now not only do we have some state that will be
    given to the Rotor from a collaborator (the key) but also some state
    that is internal only to it's own operation.
    
    We're not going to expose the value of the position to the Rotor
    objects' collaborators, because its not necessary. We're using another
    principle of object oriented design to **encapsulate** the position
    state within the rotor.
    
    We are allowing collaborators (the Enigma class) to send the message
    `translate` to a rotor, and trusting it will receive a new integer. How
    it translates it is not of the Enigma object's concern, and we've
    successfully kept the Enigma responsible to only a single actor.
    
    If the Enigma class was to contain code that used the implementation of
    the stepping mechanism, we're exposing it to the need to change if that
    process changes. That sharing of an actor is **coupling**. Two objects
    are coupled when changes in one require changes in another.
    Encapsulation is one way to avoid unneccessary coupling.

--- rotor.rb
+++ rotor.rb
@@ -1,9 +1,25 @@
 class Rotor
-  def initialize(wiring)
-    @wiring = wiring
+  def initialize(key)
+    @key = key
+    @position = 0
   end
 
   def translate(index)
-    @wiring.at(index)
+    cyphertext = step_adjusted_key.at(index)
+    advance!
+    cyphertext
+  end
+
+  def advance!
+    @position += 1
+    if @position >= @key.length
+      @position = 0
+    end
+  end
+
+  private
+
+  def step_adjusted_key
+    @key.rotate(@position)
   end
 end
--- rotor_test.rb
+++ rotor_test.rb
@@ -5,7 +5,26 @@ class Rotor::Test < Minitest::Test
   def test_translates_integer_based_on_wiring
     rotor = Rotor.new([1, 2, 0])
     assert_equal 1, rotor.translate(0)
+
+    rotor = Rotor.new([1, 2, 0])
     assert_equal 2, rotor.translate(1)
+
+    rotor = Rotor.new([1, 2, 0])
     assert_equal 0, rotor.translate(2)
+  end
+
+  def test_advance_moves_the_key_for_next_translation
+    rotor = Rotor.new([1, 2, 3, 0])
+    assert_equal 1, rotor.translate(0)
+    assert_equal 2, rotor.translate(0)
+  end
+
+  def test_advance_in_a_full_circle_goes_to_zero
+    rotor = Rotor.new([1, 2, 3, 0])
+    assert_equal 1, rotor.translate(0)
+    assert_equal 2, rotor.translate(0)
+    assert_equal 3, rotor.translate(0)
+    assert_equal 0, rotor.translate(0)
+    assert_equal 1, rotor.translate(0)
   end
 end
