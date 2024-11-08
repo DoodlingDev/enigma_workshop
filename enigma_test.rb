14fa69e8767ce47d165badb9e4285c16ae6ad1a6
Aji Slater <aji.slater@gmail.com>
2024-11-08 11:01:05 -0600

    Adds test for reflector and decryption
    The one missing piece to our emulator replicating a functional Enigma
    machine is decryption. Given the same starting settings as the machine
    that encyphered a string of characters, the Enigma machine will reverse
    the process and DEcrypt the phrase.
    
    We've added new tests in the enigma_test.rb file which will test for
    this ability. Make use of the "open for extension" property of our
    Enigma emulator and build a `ReflectorSpindle` class which can
    accomplish this task and make the tests pass.
    
    Go ahead, the next commit will still be here when you get back.

--- enigma_test.rb
+++ enigma_test.rb
@@ -3,6 +3,7 @@ require_relative "enigma"
 require_relative "keyboard"
 require_relative "lampboard"
 require_relative "spindle"
+require_relative "reflector_spindle"
 require_relative "rotor"
 require_relative "wiring"
 
@@ -57,5 +58,45 @@ class Enigma::Test < Minitest::Test
 
     encyphered_phrase = enigma.cypher("HELLOWORLD")
     assert_equal "IWLYOVBHFZ", encyphered_phrase
+  end
+
+  def test_encyphers_a_phrase_with_reflector
+    rotor_i = Rotor.new(ROTOR_I)
+    rotor_ii = Rotor.new(ROTOR_II)
+    rotor_iii = Rotor.new(ROTOR_III)
+    enigma = Enigma.new(
+      input: Keyboard.new,
+      output: Lampboard.new,
+      transformer: ReflectorSpindle.new(rotor_i, rotor_ii, rotor_iii)
+    )
+
+    encyphered_phrase = enigma.cypher("HELLOWORLD")
+    assert_equal "BORXECDZFQ", encyphered_phrase
+  end
+
+  def test_encyphers_and_decrypts_a_phrase
+    rotor_i = Rotor.new(ROTOR_I)
+    rotor_ii = Rotor.new(ROTOR_II)
+    rotor_iii = Rotor.new(ROTOR_III)
+    encypher_enigma = Enigma.new(
+      input: Keyboard.new,
+      output: Lampboard.new,
+      transformer: ReflectorSpindle.new(rotor_i, rotor_ii, rotor_iii)
+    )
+
+    starting_phrase = "WETTERBERICHT"
+    encyphered_phrase = encypher_enigma.cypher(starting_phrase)
+
+    rotor_i = Rotor.new(ROTOR_I)
+    rotor_ii = Rotor.new(ROTOR_II)
+    rotor_iii = Rotor.new(ROTOR_III)
+    decypher_enigma = Enigma.new(
+      input: Keyboard.new,
+      output: Lampboard.new,
+      transformer: ReflectorSpindle.new(rotor_i, rotor_ii, rotor_iii)
+    )
+
+    decyphered_phrase = decypher_enigma.cypher(encyphered_phrase)
+    assert_equal starting_phrase, decyphered_phrase
   end
 end
